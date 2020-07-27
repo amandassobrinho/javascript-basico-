f42932e5eb8ca32d9cd9d796e9fffc300a75213b
amandassobrinho <68513559+amandassobrinho@users.noreply.github.com>
2020-07-27 19:02:59 -0300

    Create script.js

---
+++ verificador de idade/script.js
@@ -0,0 +1,20 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getUTCFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('verifique os dados e tente novamente')
+    } else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        if(fsex[0].checked){
+        gênero = 'Homem'
+        } else if (fsex[1].checked){
+        gênero = 'Mulher'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} Anos `
+    }
+}
